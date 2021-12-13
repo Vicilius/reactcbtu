018a3cfa63819e089e241750bdc3ea7ae28195ca
Vicilius <56098903+Vicilius@users.noreply.github.com>
2021-12-13 11:17:35 -0300

    Update index.js

--- src/Telas/Inicio/index.js
+++ src/Telas/Inicio/index.js
@@ -112,6 +112,52 @@ export default class Inicio extends Component{
 
 
 
+        <View>
+            <Card style={styles.card} mode='outlined'>
+        <Card.Title></Card.Title>
+        <Card.Content>
+            <Title><Ionicons name="logo-facebook" style={styles.icon} size={20} ></Ionicons>Facebook</Title>
+         
+        </Card.Content>
+        <Card.Cover source={{uri: 'https://scontent.fplu22-1.fna.fbcdn.net/v/t39.30808-6/s960x960/240883982_289297376540398_3277008512044388971_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeFqLSACjQIsR0saeemg3psqlkcih-X43p2WRyKH5fjenTp6RAbNhJYLJC6OA1IieiUAvsP0LFint0cehLlzsBXz&_nc_ohc=yW-DkG2v4RUAX8BafO0&_nc_ht=scontent.fplu22-1.fna&oh=00_AT_LSqxRP_sy8g-ZVUglYFAFVUi5CP-ACNJ7EUKz_p7EgQ&oe=61BBE7FA'}}
+                    style={{width, height, resizeMode: 'cover' }}
+      /*  style={{width,height}} */
+        ></Card.Cover>
+        
+           
+        </Card>
+
+        
+        
+
+        </View>
+
+
+        <View>
+            <Card style={styles.card} mode='outlined'>
+        <Card.Title></Card.Title>
+        <Card.Content>
+            <Title><Ionicons name="logo-instagram" style={styles.icon} size={20} ></Ionicons>Instagram</Title>
+         
+        </Card.Content>
+        <Card.Cover source={{uri: 'https://scontent.fplu22-1.fna.fbcdn.net/v/t39.30808-6/263407090_355316866605115_8941728670164081916_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_eui2=AeG7LmFkhf2vYHD6IJ6ONMk0Y5jOBQl4LIljmM4FCXgsiRYs_11_k9_KWEByWtfIx_kRILyX5wbGcFdswYYGLmUV&_nc_ohc=rgbST_exnmAAX-ld3fm&_nc_ht=scontent.fplu22-1.fna&oh=677bfd77dcc0e76f7f6976a36c5e8ead&oe=61BBCCAA'}}
+                    style={{width, height, resizeMode: 'cover' }}
+      /*  style={{width,height}} */
+        ></Card.Cover>
+        
+           
+        </Card>
+
+        
+        
+
+        </View>
+
+
+
+
+
+
 
 
 
